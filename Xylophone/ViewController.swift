bc459aa2975eeba6289e2e18ed29a5c683dca726
ninew089 <ninew.jck@gmail.com>
2022-08-10 16:37:44 +0700

    .

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,14 +7,31 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    func playSound(resource:String) {
+        
+        let url = Bundle.main.url(forResource: resource, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(resource: sender.currentTitle!)
+        
+        
+    }
+    
     
 
 }
